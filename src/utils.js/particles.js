d41f63e589d945539bc1f5bd9175c2b09aa6b7df
Albulenalala <albulenalala5@gmail.com>
2024-08-26 16:37:10 +0200

    firstcommit

---
+++ src/utils.js/particles.js
@@ -0,0 +1,72 @@
+export default {
+  background: {
+    color: {
+      value: "rgb(2,0,36) linear-gradient(90deg, rgba(2,0,36,1) 5%, rgba(9,9,121,1) 49%, rgba(0,212,255,1) 81%, rgba(0,212,255,1) 85%)",
+    },
+  },
+  fpsLimit: 120,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "var(--selected-theme-main-color)",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outModes: {
+        default: "bounce",
+      },
+      random: false,
+      speed: 2, // Adjusted speed for slower movement
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+  detectRetina: true,
+};
